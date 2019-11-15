fb9c1136a8a5b5e76d2fbfcc4c63e06ee8d837ad
Sony Seng <sonyseng@gmail.com>
2019-11-15 10:10:33 -0600

    Remove secure attribute in cookies so browsers can set cookies when proxying to https

--- index.js
+++ index.js
@@ -64,6 +64,8 @@ class JsonCachingProxy {
 			let cookieParts = c.split(';');
 			let newCookieParts = [];
 
+      console.log(cookieParts);
+
 			cookieParts.forEach(c => {
 				if (c.indexOf('domain') === -1 && c.indexOf('secure') === -1) {
 					newCookieParts.push(c);
